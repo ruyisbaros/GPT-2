2a920c4aec5dd7d7662136855585d59ff0facd7e
Ahmet Erdonmez <ahmet.erdonmez77@gmail.com>
2024-10-18 15:24:26 +0200

    classes created

--- GPT-2/train_gbt2.py
+++ GPT-2/train_gbt2.py
@@ -7,9 +7,65 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
-    model_name: str = "gpt2"
+    model_name: str = "gpt2-400M"
     block_size: int = 256
     vocab_size: int = 65
     n_layers: int = 6
     n_head: int = 6
-    n_emd: int = 384
+    n_embd: int = 384
+
+
+class GPTModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layers)]),
+                ln_f=nn.LayerNorm(config.n_embd),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attention = CasualSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attention(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
+
+
+class CasualSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.q = nn.Linear(config.n_embd, config.n_embd, bias=True)
+        self.k = nn.Linear(config.n_embd, config.n_embd, bias=True)
+        self.v = nn.Linear(config.n_embd, config.n_embd, bias=True)
+
+        self.linear = nn.Linear(config.n_embd, config.n_embd)
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_mlp)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_mlp, config.n_embd)
+        # self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        # x = self.dropout(x)
+        x = self.c_proj(x)
+        return x
